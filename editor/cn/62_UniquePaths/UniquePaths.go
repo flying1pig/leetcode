7531f3905398deb0872e6a3c3adf5b959abb21a9
duqi <duqi@nsfocus.com>
2025-03-22 11:29:36 +0800

    62_uniquePaths

--- editor/cn/62_UniquePaths/UniquePaths.go
+++ editor/cn/62_UniquePaths/UniquePaths.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 /**
 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
 
@@ -57,13 +55,116 @@ import "fmt"
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func uniquePaths(m int, n int) int {
-	return 0
-
+	mem := make([]int, n+1)
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 && j == 0 {
+				mem[1] = 1
+				continue
+			}
+			mem[j+1] = mem[j+1] + mem[j]
+		}
+	}
+	return mem[n]
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
 
-func main() {
-	a := []int{1, 1}
-	fmt.Println(a)
+/*
+定义f(i,j)为走到(i,j)的不同路径数量
+	从上面来，f(i,j) = f(i-1,j)
+	从左边来，f(i,j) = f(i,j-1)
+状态方程: f(i,j) = f(i-1,j) + f(i,j-1)
+边界条件:
+	f(0,0) = 1
+	f(-1,j) = 0
+	f(i,-1) = 0
+*/
+
+/*
+递归:
+func uniquePaths(m int, n int) int {
+	var f func(i,j int) int
+	f = func(i, j int) int {
+		if i == 0 && j == 0 {
+			return 1
+		}
+		if i<0 || j <0 {
+			return 0
+		}
+		return f(i-1,j) + f(i,j-1)
+	}
+	return f(m-1,n-1)
 }
+时间复杂度: o(2^m*n)
+空间复杂度: o(m*n)
+*/
+
+/*
+记忆化搜索:
+func uniquePaths(m int, n int) int {
+	var f func(i,j int) int
+	mem := make([][]int,m)
+	for i:=0;i<m;i++ {
+		mem[i] = make([]int,n)
+	}
+	f = func(i, j int) int {
+		if i == 0 && j == 0 {
+			return 1
+		}
+		if i<0 || j <0 {
+			return 0
+		}
+		if mem[i][j] == 0 {
+			mem[i][j] = f(i-1,j) + f(i,j-1)
+		}
+		return mem[i][j]
+	}
+	return f(m-1,n-1)
+}
+时间复杂度：o(m*n)
+空间复杂度: o(m*n)
+*/
+
+/*
+递归:
+func uniquePaths(m int, n int) int {
+	mem := make([][]int,m+1)
+	for i:=0;i<=m;i++ {
+		mem[i] = make([]int,n+1)
+	}
+	for i:=0;i<m;i++ {
+		for j:=0;j<n;j++ {
+			if i == 0 && j == 0 {
+				mem[1][1] = 1
+				continue
+			}
+			mem[i+1][j+1] = mem[i][j+1] + mem[i+1][j]
+		}
+	}
+	return mem[m][n]
+}
+时间复杂度: o(m*n)
+空间复杂度: o(m*n)
+*/
+
+/*
+优化:
+func uniquePaths(m int, n int) int {
+	mem := make([]int,n+1)
+	for i:=0;i<m;i++ {
+		for j:=0;j<n;j++ {
+			if i == 0 && j == 0 {
+				mem[1] = 1
+				continue
+			}
+			mem[j+1] = mem[j+1] + mem[j]
+		}
+	}
+	return mem[n]
+}
+时间复杂度: o(m*n)
+空间复杂度: o(n)
+*/
+func main() {}
+
