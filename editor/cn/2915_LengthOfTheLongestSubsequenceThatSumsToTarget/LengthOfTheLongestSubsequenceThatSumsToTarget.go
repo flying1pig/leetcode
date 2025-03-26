27d8092e760722c9b94e1e94be79c593cb08e073
Administrator <duqi2013@163.com>
2025-03-26 11:45:17 +0800

    494_findTargetSumWays,2915_lengthOfLongestSubsequence

---
+++ editor/cn/2915_LengthOfTheLongestSubsequenceThatSumsToTarget/LengthOfTheLongestSubsequenceThatSumsToTarget.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/**
+给你一个下标从 0 开始的整数数组 nums 和一个整数 target 。
+
+ 返回和为 target 的 nums 子序列中，子序列 长度的最大值 。如果不存在和为 target 的子序列，返回 -1 。
+
+ 子序列 指的是从原数组中删除一些或者不删除任何元素后，剩余元素保持原来的顺序构成的数组。
+
+
+
+ 示例 1：
+
+
+输入：nums = [1,2,3,4,5], target = 9
+输出：3
+解释：总共有 3 个子序列的和为 9 ：[4,5] ，[1,3,5] 和 [2,3,4] 。最长的子序列是 [1,3,5] 和 [2,3,4] 。所以答案为 3
+ 。
+
+
+ 示例 2：
+
+
+输入：nums = [4,1,3,2,1,5], target = 7
+输出：4
+解释：总共有 5 个子序列的和为 7 ：[4,3] ，[4,1,2] ，[4,2,1] ，[1,1,5] 和 [1,3,2,1] 。最长子序列为 [1,3,2,
+1] 。所以答案为 4 。
+
+
+ 示例 3：
+
+
+输入：nums = [1,1,5,4,5], target = 3
+输出：-1
+解释：无法得到和为 3 的子序列。
+
+
+
+
+ 提示：
+
+
+ 1 <= nums.length <= 1000
+ 1 <= nums[i] <= 1000
+ 1 <= target <= 1000
+
+
+ Related Topics 数组 动态规划 👍 56 👎 0
+
+*/
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func lengthOfLongestSubsequence(nums []int, target int) int {
+	n := len(nums)
+	ans := -1
+
+	mem := make([][]int, n)
+	for i := range mem {
+		mem[i] = make([]int, target+1)
+		for j := range mem[i] {
+			mem[i][j] = -1
+		}
+	}
+
+	var f func(int, int) int
+	f = func(i int, j int) int {
+		if i < 0 {
+			if j == 0 {
+				return 0
+			}
+			return math.MinInt
+		}
+
+		if mem[i][j] != -1 {
+			return mem[i][j]
+		}
+
+		if j < nums[i] {
+			mem[i][j] = f(i-1, j)
+			return mem[i][j]
+		}
+
+		mem[i][j] = max(f(i-1, j), f(i-1, j-nums[i])+1)
+		ans = max(ans, mem[i][j])
+		return mem[i][j]
+	}
+
+	f(n-1, target)
+	return ans
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+/*
+定义f(i,j)来计算所有和为目标值方案的最大长度
+f(i,j) = max(f(i-1,j), f(i-1,j-nums[i]))
+*/
+
+/*
+记忆化搜索:
+*/
+func main() {
+	arr := []int{1, 2, 3, 4, 5}
+	fmt.Println(lengthOfLongestSubsequence(arr, 9))
+}
---
+++ editor/cn/494_TargetSum/TargetSum.go
@@ -0,0 +1,243 @@
+package main
+
+/**
+给你一个非负整数数组 nums 和一个整数 target 。
+
+ 向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+
+
+ 例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+
+
+ 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+
+
+
+ 示例 1：
+
+
+输入：nums = [1,1,1,1,1], target = 3
+输出：5
+解释：一共有 5 种方法让最终目标和为 3 。
+-1 + 1 + 1 + 1 + 1 = 3
++1 - 1 + 1 + 1 + 1 = 3
++1 + 1 - 1 + 1 + 1 = 3
++1 + 1 + 1 - 1 + 1 = 3
++1 + 1 + 1 + 1 - 1 = 3
+
+
+ 示例 2：
+
+
+输入：nums = [1], target = 1
+输出：1
+
+
+
+
+ 提示：
+
+
+ 1 <= nums.length <= 20
+ 0 <= nums[i] <= 1000
+ 0 <= sum(nums[i]) <= 1000
+ -1000 <= target <= 1000
+
+
+ Related Topics 数组 动态规划 回溯 👍 2109 👎 0
+
+*/
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func findTargetSumWays(nums []int, target int) int {
+	s := 0
+	for _, x := range nums {
+		s += x
+	}
+	s -= abs(target)
+	if s < 0 || s%2 == 1 {
+		return 0
+	}
+	m := s / 2 //背包容量
+
+	n := len(nums)
+	mem := make([][]int, n)
+	for i := range mem {
+		mem[i] = make([]int, m+1)
+		for j := range mem[i] {
+			mem[i][j] = -1
+		}
+	}
+
+	var f func(int, int) int //第一个参数是待选数组长度，第二个参数是背包容量
+	f = func(i int, c int) int {
+		if i < 0 {
+			if c == 0 {
+				return 1
+			}
+			return 0
+		}
+		if mem[i][c] != -1 {
+			return mem[i][c]
+		}
+		if c < nums[i] { //不选
+			mem[i][c] = f(i-1, c)
+			return mem[i][c]
+		}
+		mem[i][c] = f(i-1, c) + f(i-1, c-nums[i])
+		return mem[i][c] //选和不选
+	}
+	return f(n-1, m)
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return i
+	}
+	return i
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+/*
+这个题要先做一下数学转换。
+问题等价于从原数组中取几个数，使得和为m = (s- |target|)/2，如果m小于0或者不为整数，则原问题没得答案。
+*/
+
+/*
+func findTargetSumWays(nums []int, target int) int {
+	s := 0
+	for _, x := range nums {
+		s += x
+	}
+	s -= abs(target)
+	if s < 0 || s%2 == 1 {
+		return 0
+	}
+	m := s / 2 //背包容量
+
+	n := len(nums)
+	mem := make([][]int, n)
+	for i := range mem {
+		mem[i] = make([]int, m+1)
+		for j := range mem[i] {
+			mem[i][j] = -1
+		}
+	}
+
+	var f func(int, int) int //第一个参数是待选数组长度，第二个参数是背包容量
+	f = func(i int, c int) int {
+		if i < 0 {
+			if c == 0 {
+				return 1
+			}
+			return 0
+		}
+		if mem[i][c] != -1 {
+			return mem[i][c]
+		}
+		if c < nums[i] {	//不选
+			return f(i-1,c)
+		}
+		return f(i-1,c) + f(i-1,c-nums[i])	//选和不选
+	}
+	return f(n-1,m)
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return i
+	}
+	return i
+}
+时间复杂度: o(nm), n为数组长度，m为数组元素和减去target的绝对值。
+空间复杂度: o(nm)
+*/
+
+/*
+func findTargetSumWays(nums []int, target int) int {
+	s := 0
+	for _, x := range nums {
+		s += x
+	}
+	s -= abs(target)
+	if s < 0 || s%2 == 1 {
+		return 0
+	}
+	m := s / 2 //背包容量
+
+	n := len(nums)
+	mem := make([][]int, n+1)
+	for i := range mem {
+		mem[i] = make([]int, m+1)
+	}
+
+	mem[0][0] =1
+	for i,x := range nums {
+		for c :=0;c<=m;c++ {
+			if c <x {
+				mem[i+1][c] = mem[i][c]  //不选
+			} else {
+				mem[i+1][c] = mem[i][c] + mem[i][c-x]
+			}
+		}
+	}
+	return mem[n][m]
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return i
+	}
+	return i
+}
+时间复杂度: o(mn)
+空间复杂度: o(mn)
+*/
+
+/*
+空间优化:
+
+	func findTargetSumWays(nums []int, target int) int {
+		s := 0
+		for _, x := range nums {
+			s += x
+		}
+		s -= abs(target)
+		if s < 0 || s%2 == 1 {
+			return 0
+		}
+		m := s / 2 //背包容量
+
+		n := len(nums)
+		mem := make([][]int, 2)
+		for i := range mem {
+			mem[i] = make([]int, m+1)
+		}
+
+		mem[0][0] = 1
+		for i, x := range nums {
+			for c := 0; c <= m; c++ {
+				if c < x {
+					mem[(i+1)%2][c] = mem[i%2][c] //不选
+				} else {
+					mem[(i+1)%2][c] = mem[i%2][c] + mem[i%2][c-x] //不选+选
+				}
+			}
+		}
+		return mem[n%2][m]
+	}
+
+	func abs(i int) int {
+		if i < 0 {
+			return i
+		}
+		return i
+	}
+
+时间复杂度: o(nm)
+空间复杂度: o(m)
+*/
+func main() {
+
+}
